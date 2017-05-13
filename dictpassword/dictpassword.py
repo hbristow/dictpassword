78c2a946bf08c40cba0350d6285780a7dbce1124
Hilton Bristow <hilton.bristow@gmail.com>
2017-05-13 23:17:10 +1000

    Complete rewrite of generator

--- dictpassword/dictpassword.py
+++ dictpassword/dictpassword.py
@@ -1,61 +1,91 @@
-#!/usr/bin/env python
+#
+# Dictpassword
+#
+"""
+Cryptographically secure passphrase generator.
+
+Dictpassword generates passphrases from uniformly sampled dictionary words.
+It takes care to use a cryptographic quality source of randomness. On
+UNIX-based machines, it uses the output of /dev/urandom, which gathers
+environmental noise from keystrokes, network traffic, etc.
+
+Passphrases are like passwords, except that they contain only words rather
+than symbols or numbers. They rely on the large sample space of a dictionary
+for security.
+"""
+
+from __future__ import absolute_import
+import argparse
 import math
 import os
 import random
 import sys
 
-class DictPassword(object):
-  def __init__(self, wordlist_file='/usr/share/dict/words'):
-    """Construct a DictPassword object
 
-    Keyword Args:
-      wordlist_file (str): a file to draw words from. It defaults to
-        /usr/share/dict/words
-    """
-    self.randomizer = random.SystemRandom()
-    with open(wordlist_file, 'r') as f:
-      self.words = [line.rstrip().lower() for line in f]
+class Passphrase(object):
+    """A passphrase generator from an underlying wordlist.
 
-  def gen(self, N):
-    """Generate a password from N randomly chosen words
-
-    The generate function uses Python's random.SystemRandom() class
-    which provides a true cryptographically secure source of entropy
-
-    Args:
-      N (int): the number of words to draw
+    This class represents a passphrase generator, which samples from a
+    wordlist. The random number generator is random.SystemRandom, which
+    is a non-repeatable random device.
     """
 
-    return [self.randomizer.choice(self.words) for n in range(0,N)]
+    def __init__(self, wordlist_file='/usr/share/dict/words'):
+        """Initialize a new Passphrase instance.
+
+        :param wordlist_file: The path to the words to sample.
+        :return: A Passphrase instance.
+        """
+        self.random = random.SystemRandom()
+        with open(wordlist_file) as f:
+            self.words = [line.rstrip().lower() for line in f]
+
+    def generate(self, N):
+        """Generate a passphrase from N randomly chosen words.
+
+        :param N: The number of words to randomly sample:
+        :return: A tuple of the sampled words, in selection order.
+        """
+        return tuple(self.random.choice(self.words) for _ in range(N))
+
 
 def main():
-  from argparse import ArgumentParser
+    """The main command-line entrypoint."""
 
-  # module path
-  module = os.path.dirname(os.path.abspath(__file__))
+    # Module path.
+    module = os.path.dirname(os.path.abspath(__file__))
 
-  # parse the input arguments
-  parser = ArgumentParser(description='Generates a password from uniformly sampled dictionary words')
-  parser.add_argument('-w', '--wordlist', default='common', 
-    help="A custom wordlist file containing one word per line. Use 'common' or 'full' to access "
-         "builtin english wordlists with 60,000 and 235,000 words respectively. (Default common)")
-  parser.add_argument('N', metavar='N', type=int, nargs='?', default=4,
-    help='The number of random trials (words)')
-  args = parser.parse_args()
+    # Construct an argument parser.
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
 
-  # compute the wordlist being used
-  if args.wordlist in ['common', 'full']:
-    args.wordlist = os.path.join(module, args.wordlist)
+    parser.add_argument('-s', '--separator', default='-',
+        help='The separator to insert between words (default: "-")')
+    parser.add_argument('-w', '--wordlist', default='common',
+        help=('A custom wordlist file containing one word per line. '
+              'Use "common" or "full" to access builtin English wordlists '
+              'with 60,000 and 235,000 words respectively. (Default: common)'))
+    parser.add_argument('N', type=int, nargs='?', default=4,
+        help='The number of words. (default: 4)')
 
-  # generate the password
-  dp = DictPassword(args.wordlist)
-  password = dp.gen(args.N)
+    # Parse the arguments.
+    args = parser.parse_args()
 
-  # compute the password entropy
-  entropy = len(password) * math.log(len(dp.words), 2)
+    # Compute the wordlist being used.
+    if args.wordlist in ('common', 'full'):
+        args.wordlist = os.path.join(module, args.wordlist)
 
-  # display the password to the user
-  print('Passphrase: {0}\nEntropy: ~{1} bits'.format('-'.join(password), str(entropy)))
+    # Generate the passphrase.
+    generator = Passphrase(args.wordlist)
+    passphrase = generator.generate(args.N)
+
+    # Compute the passphrase entropy.
+    entropy = len(passphrase) * math.log(len(generator.words), 2)
+
+    # Output the passphrase to the user.
+    OUTPUT_SPEC = 'Passphrase: {}\nEntropy: ~{} bits'
+    print(OUTPUT_SPEC.format(args.separator.join(passphrase), entropy))
+
 
 if __name__ == '__main__':
-  main()
+    main()
